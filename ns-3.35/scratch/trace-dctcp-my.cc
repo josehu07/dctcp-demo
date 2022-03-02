e021fa3b3c1e38cfd71978cde22cf1e2b00715e0
Guanzhou Hu <huguanzhou123@sina.com>
2022-03-02 16:58:27 -0600

    also log DCTCP alpha

--- ns-3.35/scratch/trace-dctcp-my.cc
+++ ns-3.35/scratch/trace-dctcp-my.cc
@@ -313,7 +313,8 @@ main (int argc, char *argv[])
   for (size_t i = 0; i < 20; ++i)
     sinks[i]->TraceConnectWithoutContext ("Rx", MakeBoundCallback (&TraceRxSinkBytes, i));
   txSockets[0]->TraceConnectWithoutContext ("CongestionWindow", MakeCallback (&TraceCwndSizeS0));
-  txSockets[0]->m_congestionControl->TraceConnectWithoutContext ("DctcpAlpha", MakeCallback (&TraceDctcpAlpha));
+  Ptr<TcpSocketBase> tcp_socket = txSockets[0]->GetObject<TcpSocketBase> ();
+  tcp_socket->m_congestionControl->TraceConnectWithoutContext ("DctcpAlpha", MakeCallback (&TraceDctcpAlpha));
   printf("%7s  %11s  %13s  %13s  %5s\n",
          "Time(s)", "Queue(pkts)", "CwndS0(bytes)", "RxSink(bytes)", "alpha");
   Simulator::Schedule (progressInterval, &PrintProgress, progressInterval, queues.Get (0));
